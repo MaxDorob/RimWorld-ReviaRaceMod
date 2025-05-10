13beeafb18451a06577234b08d203a623c84a685
Макс Дороболюк <dmv_01@mail.ru>
2025-05-10 20:10:36 +0600

    Spawn somewhere bloodstone as reward

---
+++ Source/Main/Rituals/RitualOutcomeEffectWorker_Sacrificed.cs
@@ -0,0 +1,29 @@
+﻿using ReviaRace.Helpers;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ReviaRace.Rituals
+{
+    public class RitualOutcomeEffectWorker_Sacrificed : RitualOutcomeEffectWorker_Consumable
+    {
+
+        public RitualOutcomeEffectWorker_Sacrificed() : base() { }
+        public RitualOutcomeEffectWorker_Sacrificed(RitualOutcomeEffectDef def) : base(def) { }
+        public override void Apply(float progress, Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual)
+        {
+            base.Apply(progress, totalPresence, jobRitual);
+            var sacrificer = totalPresence.Keys.First();
+            var position = sacrificer.Position;
+            var thing = ThingMaker.MakeThing(Defs.Bloodstone);
+            thing.stackCount = 2;
+
+            GenPlace.TryPlaceThing(thing, position, sacrificer.Map, ThingPlaceMode.Near);
+        }
+    }
+}
