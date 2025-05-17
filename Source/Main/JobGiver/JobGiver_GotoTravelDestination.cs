8412e46c0ac129190b54a7a23b6ef7a077a0fb20
MaxDorob <maxdor81@gmail.com>
2025-05-18 04:46:20 +0600

    Sacrificer will stay right on prisoner (spams errors tho)

---
+++ Source/Main/JobGiver/JobGiver_GotoTravelDestination.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace ReviaRace.JobGiver
+{
+    public class JobGiver_GotoTravelDestination : RimWorld.JobGiver_GotoTravelDestination
+    {
+        public JobGiver_GotoTravelDestination() : base()
+        {
+        }
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            var result = base.TryGiveJob(pawn);
+            if(result != null)
+            {
+                result.collideWithPawns = true;
+            }
+
+            Log.Message($"{base.GetDestination(pawn)} {result == null} {result?.targetA}");
+            return result;
+        }
+    }
+}
