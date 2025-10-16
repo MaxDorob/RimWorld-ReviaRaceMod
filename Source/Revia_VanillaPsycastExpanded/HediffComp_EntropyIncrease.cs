ea4c1c4689896402c047200ba544d3fbd3bdc604
MaxDorob <maxdor81@gmail.com>
2025-10-17 01:29:32 +0600

    some optimization?

--- Source/Revia_VanillaPsycastExpanded/HediffComp_EntropyIncrease.cs
+++ Source/Revia_VanillaPsycastExpanded/HediffComp_EntropyIncrease.cs
@@ -2,20 +2,18 @@ using Verse;
 
 namespace ReviaRace
 {
-	public class HediffComp_EntropyIncrease : HediffComp
-	{
-        public override void CompPostTick(ref float severityAdjustment)
+    public class HediffComp_EntropyIncrease : HediffComp
+    {
+        public override void CompPostTickInterval(ref float severityAdjustment, int delta)
         {
-            base.CompPostTick(ref severityAdjustment);
-			var pawn = parent.pawn;
-			if (Find.TickManager.TicksGame % 10 == 0)
-			{
-				pawn.psychicEntropy.TryAddEntropy(1f, null, true, true);
-			}
-			if (pawn.psychicEntropy.EntropyValue >= pawn.psychicEntropy.MaxEntropy)
-			{
-				pawn.MentalState.RecoverFromState();
-			}
-		}
+            base.CompPostTickInterval(ref severityAdjustment, delta);
+            var pawn = parent.pawn;
+            pawn.psychicEntropy.TryAddEntropy(delta / 10f, null, true, true);
+
+            if (pawn.psychicEntropy.EntropyValue >= pawn.psychicEntropy.MaxEntropy)
+            {
+                pawn.MentalState.RecoverFromState();
+            }
+        }
     }
 }
