262657691515c16fde42b4551fae9b239fa6b3cf
MaxDorob <maxdor81@gmail.com>
2025-10-18 21:44:30 +0600

    Xenotype conversion fixes

--- Source/Main/Rituals/RitualOutcomeEffectWorker_XenotypeConversion.cs
+++ Source/Main/Rituals/RitualOutcomeEffectWorker_XenotypeConversion.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace ReviaRace.Rituals
@@ -16,6 +17,41 @@ namespace ReviaRace.Rituals
         protected override void ApplyExtraOutcome(Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual, RitualOutcomePossibility outcome, out string extraOutcomeDesc, ref LookTargets letterLookTargets)
         {
             base.ApplyExtraOutcome(totalPresence, jobRitual, outcome, out extraOutcomeDesc, ref letterLookTargets);
+
+            var sacrificer = jobRitual.PawnWithRole("sacrificer");
+
+            var thingDefToCarry = Defs.Bloodstone;
+
+            var totalCountToRemove = 10;
+
+            var carriedThing = sacrificer.carryTracker.CarriedThing;
+            if (carriedThing?.def == thingDefToCarry)
+            {
+                var countToRemove = Mathf.Min(totalCountToRemove, carriedThing.stackCount);
+                carriedThing.stackCount -= countToRemove;
+                if (carriedThing.stackCount <= 0)
+                {
+                    carriedThing.Destroy();
+                }
+                totalCountToRemove -= countToRemove;
+            }
+            while (totalCountToRemove > 0)
+            {
+                var thing = sacrificer.inventory.innerContainer.FirstOrDefault(x => x.def == thingDefToCarry);
+                if (thing == null)
+                {
+                    Log.Error($"Can't find enough {thingDefToCarry}. Left to remove {totalCountToRemove}");
+                    break;
+                }
+                var countToRemove = Mathf.Min(totalCountToRemove, thing.stackCount);
+                thing.stackCount -= countToRemove;
+                if (thing.stackCount <= 0)
+                {
+                    thing.Destroy();
+                }
+                totalCountToRemove -= countToRemove;
+            }
+
             var blessedPawn = jobRitual.PawnWithRole("convertee");
             if (outcome.Positive)
             {
