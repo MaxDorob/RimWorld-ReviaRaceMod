59507a70a33191438ef2cc63f122235a288bfe13
MaxDorob <maxdor81@gmail.com>
2025-10-26 20:14:48 +0600

    Changed psycast level up system

--- Source/Revia_VanillaPsycastExpanded/Startup.cs
+++ Source/Revia_VanillaPsycastExpanded/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using ReviaRace.Rituals;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,13 +23,24 @@ namespace Revia_VanillaPsycastExpanded
             RitualOutcomeEffectWorker_PsycastBlessing.BlessedWithPsycast += BlessedWithPsycast;
         }
 
-        private static void BlessedWithPsycast(Pawn pawn)
+        private static void BlessedWithPsycast(RitualOutcomeEffectWorker_PsycastBlessing.PsycastBlessingEventArgs eventArgs)
         {
-            var psycasts = pawn.Psycasts();
+            var psycasts = eventArgs.pawn.Psycasts();
+            if (psycasts == null)
+            {
+                eventArgs.pawn.ChangePsylinkLevel(1);
+                psycasts = eventArgs.pawn.Psycasts();
+            }
+            else
+            {
+                psycasts.GainExperience(350f * eventArgs.positivityLevel);
+            }
+
             if (psycasts != null && !psycasts.unlockedPaths.Contains(Defs.Revia_BloodPath))
             {
                 psycasts.UnlockPath(Defs.Revia_BloodPath);
             }
+            eventArgs.handled = true;
         }
     }
 }
