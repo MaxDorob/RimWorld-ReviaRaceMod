17035e82211d0b76789b21e89b3e99bc50ba5594
MaxDorob <maxdor81@gmail.com>
2025-10-28 01:57:39 +0600

    Revia psycasters in raid, hell yeah!

---
+++ Source/Revia_VanillaPsycastExpanded/BloodyExplosion/Ability_BloodyExplosion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Revia_VanillaPsycastExpanded
+{
+    public class Ability_BloodyExplosion : VEF.Abilities.Ability
+    {
+        public override bool AICanUseOn(Thing target)
+        {
+            return base.AICanUseOn(target) && target is Pawn pawn && pawn.health.hediffSet.BleedRateTotal > 0.5f;
+        }
+    }
+}
